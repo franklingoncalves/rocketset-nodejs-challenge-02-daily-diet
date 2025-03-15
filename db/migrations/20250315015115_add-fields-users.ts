8f44a8f159c9154508d7609ed1afd0b847881bda
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-03-14 23:01:34 -0300

    feat: add `created_at` and `updated_at` fields to users table

---
+++ db/migrations/20250315015115_add-fields-users.ts
@@ -0,0 +1,16 @@
+import { table } from 'console';
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.timestamp('create_at').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dropColumn('create_at');
+    table.dropColumn('updated_at');
+  });
+}
