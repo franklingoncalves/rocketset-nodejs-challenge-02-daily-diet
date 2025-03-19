6e88a4f18f46080216baf914f3bd3b8a1ba3396c
Franklin Gonçalves <franklingoncalves@gmail.com>
2025-03-19 20:26:34 -0300

    feat: add `POST` `/meals` route for meal creation

---
+++ src/routes/meals.routes.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
+import { randomUUID } from 'node:crypto';
+import { knex } from '../database';
+
+const mealSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  isOnDiet: z.boolean(),
+});
+
+async function insertMeal(mealData, userId) {
+  await knex('meals').insert({
+    id: randomUUID(),
+    name: mealData.name,
+    description: mealData.description,
+    is_on_diet: mealData.isOnDiet,
+    user_id: userId,
+    create_at: new Date(),
+    updated_at: new Date(),
+  });
+}
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', checkSessionIdExists);
+
+  app.post('/', async (request, reply) => {
+    try {
+      const mealData = mealSchema.parse(request.body);
+      const userId = request.user?.id;
+      await insertMeal(mealData, userId);
+      return reply.status(201).send();
+    } catch (error) {
+      return reply.status(400).send({ error: error.message });
+    }
+  });
+}
